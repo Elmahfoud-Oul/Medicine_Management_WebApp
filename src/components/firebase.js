234313de6f0f0d142608a5b26eb3f2d8e64e71d8
Elmahfoud & 马云 <mahfodoulhadj8@gmail.com>
2025-12-13 00:20:49 +0100

    firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -5,15 +5,16 @@ import {getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Try to chnage this info here with your own firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDrFj6lnNq-edCGXomdoAQXmqjpRDWB3cg",
-  authDomain: "my-medicine-remminder.firebaseapp.com",
-  projectId: "my-medicine-remminder",
-  storageBucket: "my-medicine-remminder.firebasestorage.app",
-  messagingSenderId: "943004971514",
-  appId: "1:943004971514:web:6d4feed1922ab26f31f443"
+  apiKey: " ",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: "",
+  appId: " "
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
